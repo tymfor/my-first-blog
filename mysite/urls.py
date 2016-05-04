1987309fa617856e5206ffa34e2257d7d9c93943
jungchan.choi@gmail.com <JCCHOI@Jung-Chan-CHOIui-MacBook-Air.local>
2016-05-05 01:40:30 +0200

    before wrapping bootstrap

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+    # url(r'', include('blog.urls')),
+    url(r'', include('qna.urls')),
 ]
