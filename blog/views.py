21f9d7df207eb2052eb5462fc4658aa3fc56d654
jungchan.choi@gmail.com <JCCHOI@Jung-Chan-CHOIui-MacBook-Air.local>
2016-05-05 02:53:28 +0200

    applying bootstrap

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/index.html', {'posts':posts})
 
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
