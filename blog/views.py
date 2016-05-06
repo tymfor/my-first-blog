b56bb2706a3bec194c842783dce5088734b6e083
Jung Chan Choi <JCC@ngi.no>
2016-05-06 16:40:42 +0200

    form is added

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from . models import Post
-from django.shortcuts import render, get_object_or_404
+from . forms import Postform
+
 
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
     return render(request, 'blog/index.html', {'posts':posts})
 
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_about(request):
+    return render(request, 'blog/about.html')
+
+def post_new(request):
+    if request.method == 'POST':
+        form = Postform(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail',pk=post.pk)
+    else:
+        form = Postform()
+    return render(request, 'blog/post_edit.html',{'form':form})
+
+def post_edit(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = Postform(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail',pk=post.pk)
+    else:
+        form = Postform(instance=post)
+    return render(request, 'blog/post_edit.html',{'form':form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'qna',
 ]
 
 MIDDLEWARE_CLASSES = [
